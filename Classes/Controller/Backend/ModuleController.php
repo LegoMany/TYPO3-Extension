183a785ed91de56b14049356f4b31018ae31faf5
Sorin Loghin <sorin.loghin@in2code.de>
2022-10-31 02:22:23 +0200

    [TASK] Add backend module

---
+++ Classes/Controller/Backend/ModuleController.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace In2code\Extension\Controller\Backend;
+
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class ModuleController extends ActionController
+{
+
+}
--- ext_tables.php
+++ ext_tables.php
@@ -3,4 +3,18 @@
 defined('TYPO3_MODE') or die();
 
 (static function () {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        'In2code.extension',
+        'tools',
+        'Module1',
+        '',
+        [
+            \In2code\Extension\Controller\Backend\ModuleController::class => 'index'
+        ],
+        [
+            'access' => 'user,group',
+            'icon' => 'EXT:in2solr/Resources/Public/Icons/module_status.svg',
+            'labels' => 'LLL:EXT:extension/Resources/Private/Language/locallang.xlf',
+        ]
+    );
 })();
