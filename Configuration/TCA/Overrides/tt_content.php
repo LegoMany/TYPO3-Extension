5deb76dd9ed990a72f2e7128ff69f7c5d0a57e90
Sorin Loghin <sorin.loghin@in2code.de>
2022-10-31 02:17:54 +0200

    [TASK] Add flexform

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -8,3 +8,9 @@ defined('TYPO3_MODE') or die();
     'Plugin title',
     'extension_icon'
 );
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['extension_pluginname'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'extension_pluginname',
+    'FILE:EXT:extension/Configuration/FlexForm/PluginName.xml'
+);
